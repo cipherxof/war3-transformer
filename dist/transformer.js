8eff47b8f3b848e039ebbb1a8f8e098aaa00464f
trigger <triggerhappy187@live.com>
2020-03-31 11:40:53 -0700

    change target to es5

--- dist/transformer.js
+++ dist/transformer.js
@@ -1,12 +1,14 @@
-import * as nodePath from "path";
-import { createMatchPath } from "tsconfig-paths";
-import * as utils from "tsutils";
-import * as ts from "typescript";
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var nodePath = require("path");
+var tsconfig_paths_1 = require("tsconfig-paths");
+var utils = require("tsutils");
+var ts = require("typescript");
 require.extensions[".ts"] = require.extensions[".js"];
 require.extensions[".tsx"] = require.extensions[".js"];
-let absoluteBaseUrl;
-let compilerOptions;
-let matchPathFunc;
+var absoluteBaseUrl;
+var compilerOptions;
+var matchPathFunc;
 function getModuleSpecifierValue(specifier) {
     return specifier.getText().substr(specifier.getLeadingTriviaWidth(), specifier.getWidth() - specifier.getLeadingTriviaWidth() * 2);
 }
@@ -17,29 +19,29 @@ function shouldAddCurrentWorkingDirectoryPath(baseUrl) {
     if (!baseUrl) {
         return true;
     }
-    const worksOnUnix = baseUrl[0] === "/";
-    const worksOnWindows = new RegExp("^[A-Z]:/").test(baseUrl);
+    var worksOnUnix = baseUrl[0] === "/";
+    var worksOnWindows = new RegExp("^[A-Z]:/").test(baseUrl);
     return !(worksOnUnix || worksOnWindows);
 }
 ;
-export default function runTransformer(program) {
-    const checker = program.getTypeChecker();
+function runTransformer(program) {
+    var checker = program.getTypeChecker();
     function processNode(node, file) {
         if (utils.isCallExpression(node)) {
-            const signature = checker.getResolvedSignature(node);
-            const decl = signature.declaration;
+            var signature = checker.getResolvedSignature(node);
+            var decl = signature.declaration;
             if (!decl)
                 return;
             if (decl.kind == ts.SyntaxKind.FunctionDeclaration) {
-                const funcName = decl.name.escapedText;
+                var funcName = decl.name.escapedText;
                 if (funcName === "compiletime") {
-                    const argument = node.arguments[0];
-                    const codeBlock = argument.getFullText();
-                    let transpiledJs = ts.transpile(codeBlock).trimRight();
+                    var argument = node.arguments[0];
+                    var codeBlock = argument.getFullText();
+                    var transpiledJs = ts.transpile(codeBlock).trimRight();
                     if (transpiledJs[transpiledJs.length - 1] === ";") {
                         transpiledJs = transpiledJs.substr(0, transpiledJs.length - 1);
                     }
-                    const result = eval(`(${transpiledJs})()`);
+                    var result = eval("(" + transpiledJs + ")()");
                     if (utils.isVariableDeclaration(node.parent) || utils.isExportAssignment(node.parent)) {
                         return ts.createStringLiteral(result);
                     }
@@ -51,22 +53,22 @@ export default function runTransformer(program) {
             if (!node.moduleSpecifier || !node.moduleSpecifier.getSourceFile()) {
                 return node;
             }
-            const sourceFilePath = nodePath.dirname(file.fileName);
-            const specifierValue = getModuleSpecifierValue(node.moduleSpecifier);
-            const matchedPath = matchPathFunc(specifierValue);
+            var sourceFilePath = nodePath.dirname(file.fileName);
+            var specifierValue = getModuleSpecifierValue(node.moduleSpecifier);
+            var matchedPath = matchPathFunc(specifierValue);
             if (!matchedPath) {
                 return node;
             }
-            const newNode = ts.getMutableClone(node);
-            const replacePath = nodePath.relative(sourceFilePath, matchedPath).replace(/\\/g, "/");
-            newNode.moduleSpecifier = ts.createLiteral(isPathRelative(replacePath) ? replacePath : `./${replacePath}`);
+            var newNode = ts.getMutableClone(node);
+            var replacePath = nodePath.relative(sourceFilePath, matchedPath).replace(/\\/g, "/");
+            newNode.moduleSpecifier = ts.createLiteral(isPathRelative(replacePath) ? replacePath : "./" + replacePath);
             return newNode;
         }
         return;
     }
     function processSourceFile(context, file) {
         function visitor(node) {
-            const newNode = processNode(node, file);
+            var newNode = processNode(node, file);
             if (newNode != undefined) {
                 return newNode;
             }
@@ -75,18 +77,18 @@ export default function runTransformer(program) {
         return ts.visitEachChild(file, visitor, context);
     }
     function processAndUpdateSourceFile(context, file) {
-        const updatedNode = processSourceFile(context, file);
+        var updatedNode = processSourceFile(context, file);
         return ts.updateSourceFileNode(file, updatedNode.statements, updatedNode.isDeclarationFile, updatedNode.referencedFiles, updatedNode.typeReferenceDirectives, updatedNode.hasNoDefaultLib);
     }
-    return context => (node) => {
+    return function (context) { return function (node) {
         compilerOptions = context.getCompilerOptions();
         absoluteBaseUrl = shouldAddCurrentWorkingDirectoryPath(compilerOptions.baseUrl)
             ? nodePath.join(process.cwd(), compilerOptions.baseUrl || ".")
             : compilerOptions.baseUrl || ".";
-        matchPathFunc = createMatchPath(absoluteBaseUrl, compilerOptions.paths || {});
+        matchPathFunc = tsconfig_paths_1.createMatchPath(absoluteBaseUrl, compilerOptions.paths || {});
         try {
             if (ts.isBundle(node)) {
-                const newFiles = node.sourceFiles.map(file => processAndUpdateSourceFile(context, file));
+                var newFiles = node.sourceFiles.map(function (file) { return processAndUpdateSourceFile(context, file); });
                 return ts.updateBundle(node, newFiles);
             }
             else if (ts.isSourceFile(node)) {
@@ -98,5 +100,6 @@ export default function runTransformer(program) {
             throw e;
         }
         return node;
-    };
+    }; };
 }
+exports.default = runTransformer;
