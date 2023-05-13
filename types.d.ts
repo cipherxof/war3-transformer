4273860aaabc686721de60774931b94fba9163ac
trigger <triggerhappy187@live.com>
2023-05-13 00:01:31 -0700

    another fixup

--- types.d.ts
+++ types.d.ts
@@ -3,13 +3,13 @@
 declare type ObjectData = import('war3-objectdata-th').ObjectData;
 
 declare type ConstantsType = {
-  abilities: import('war3-objectdata-th').Abilities;
-  buffs: import('war3-objectdata-th').Buffs;
-  destructables: import('war3-objectdata-th').Destructables;
-  doodads: import('war3-objectdata-th').Doodads;
-  items: import('war3-objectdata-th').Items;
-  units: import('war3-objectdata-th').Units;
-  upgrades: import('war3-objectdata-th').Upgrades;
+  abilities: typeof import('war3-objectdata-th').Abilities;
+  buffs: typeof import('war3-objectdata-th').Buffs;
+  destructables: typeof import('war3-objectdata-th').Destructables;
+  doodads: typeof import('war3-objectdata-th').Doodads;
+  items: typeof import('war3-objectdata-th').Items;
+  units: typeof import('war3-objectdata-th').Units;
+  upgrades: typeof import('war3-objectdata-th').Upgrades;
 }
 
 declare interface CompiletimeContext {
